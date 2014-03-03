ebb4e880e1464231ba72d1d3ba3856b4c87cecec
mlange806 <mlange806@gmail.com>
2014-03-03 01:49:34 +0000

    Update login.php
    Split the query to give a more specific error message. Added json_encodes for Android use. Closed the connection at the end.

--- login.php
+++ login.php
@@ -1,33 +1,54 @@
 <? php
 
-	#Login script. 
-	#Uses the inputs email_login and password_login to
-	#search for a user. If found, they are taken to login_success.php
-	#If not, they are shown an error message. 
+	/*
+	  Login Script
+	  
+	  Takes email and password input and using json_encode 
+	  returns 0 for a correct login, -1 for an incorrect email
+	  and -2 for an incorrect password.
+	  
+	  WARNING: No protection against SQL injections as of yet.*/
 
-  config.php
+	config.php
 	
-	$email = $_POST['email_login'];	
+	$email    = $_POST['email_login'];	
 	$password = $_POST['password_login'];
 	
 	#TODO Protect against SQL injection and any other security risks
+	#Look into prepared statements: PDO and mysqli
 	
-	#Encrypt password
-	$password = md5($password);
 	
-  $sql = mysql_query("SELECT * 
-                      FROM User_Login 
-                      WHERE email    = '$email' and 
-                            password = '$password';");
-
-  numRows = mysql_size(sql);
+	#Encrypt password with PHP 5.5
+	$password = password_hash($password, PASSWORD_BCRYPT);
 	
-	if(numRows>0){
-		header("location:login_success.php");
+	
+	#Perform seperate queries to give specific error messages
+  	$sql_em = mysql_query("SELECT * 
+                    	   FROM User_Login 
+                    	   WHERE email    = '$email';");
+                            
+  	$sql    = mysql_query("SELECT * 
+                		   FROM User_Login 
+                		   WHERE email    = '$email' and
+                                 password = '$password';");
+	
+	$numRows_em = mysql_size($sql_em);
+  	$numRows    = mysql_size($sql);
+	
+	#Return 0 for correct email and password, -1 for incorrect email, 
+	#and -2 for incorrect password.
+	if($numRows>1){
+		echo json_encode(0);
+	}
+	
+	else if($numRows_em == 0){
+		echo json_encode(-1);
 	}
 
 	else{
-		echo "Login failed.";
+		echo json_encode(-2);
 	}
+	
+	mysql_close();
 
 ?>
