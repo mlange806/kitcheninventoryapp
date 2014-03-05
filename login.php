59661fe589ff3212a46ad3b0d97143c2ff17821f
mlange806 <mlange806@gmail.com>
2014-03-04 18:10:11 -0600

    Update login.php
    Edited queries to use prepared statements with PDO to prevent SQL injections.

--- login.php
+++ login.php
@@ -1,7 +1,6 @@
 <? php
 
-	/*
-	  Login Script
+	/*Login Script
 	  
 	  Takes email and password input and using json_encode 
 	  returns 0 for a correct login, -1 for an incorrect email
@@ -14,30 +13,35 @@
 	$email    = $_POST['email_login'];	
 	$password = $_POST['password_login'];
 	
-	#TODO Protect against SQL injection and any other security risks
-	#Look into prepared statements: PDO and mysqli
-	
-	
 	#Encrypt password with PHP 5.5
 	$password = password_hash($password, PASSWORD_BCRYPT);
 	
 	
-	#Perform seperate queries to give specific error messages
-  	$sql_em = mysql_query("SELECT * 
-                    	   FROM User_Login 
-                    	   WHERE email    = '$email';");
+	#Perform seperate queries to give specific error messages,
+	#Prepare statements using PHP Data Objects to prevent SQL injection
+  	$stm = $dbh->prepare("SELECT * 
+                    	  FROM User_Login 
+                    	  WHERE email    = ?;");
+                    	     
+    $stm->execute($email); 
+    
+    $sql_em = $stm->fetch();
                             
-  	$sql    = mysql_query("SELECT * 
-                		   FROM User_Login 
-                		   WHERE email    = '$email' and
-                                 password = '$password';");
+  	$stm    = $dbh->prepare("SELECT * 
+                		     FROM User_Login 
+                		     WHERE email    = ? and
+                                   password = ?;");
+    
+    $stm->execute(array($email, $password));
+                                   
+    $sql = stm->fetch();
 	
 	$numRows_em = mysql_size($sql_em);
   	$numRows    = mysql_size($sql);
 	
 	#Return 0 for correct email and password, -1 for incorrect email, 
 	#and -2 for incorrect password.
-	if($numRows>1){
+	if($numRows>0){
 		echo json_encode(0);
 	}
 	
