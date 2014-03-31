5eebc835aed5670fd8766a33680ee926db064530
Mark <mlange806@gmail.com>
2014-03-30 22:03:48 -0500

    Added Recipe class and activity_view_recipes.

--- KitchenInventory/gen/com/example/kitcheninventory/R.java
+++ KitchenInventory/gen/com/example/kitcheninventory/R.java
@@ -24,19 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f08000a;
         public static final int bregister=0x7f080003;
         public static final int bsubmit=0x7f080000;
         public static final int cpass=0x7f080007;
         public static final int email=0x7f080004;
         public static final int etpass=0x7f080001;
         public static final int etuser=0x7f080002;
+        public static final int listView1=0x7f080008;
         public static final int pass=0x7f080006;
+        public static final int textView1=0x7f080009;
         public static final int vemail=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_register=0x7f030001;
+        public static final int activity_view_recipes=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
---
+++ KitchenInventory/src/com/example/kitcheninventory/Recipe.java
@@ -0,0 +1,51 @@
+package com.example.kitcheninventory;
+
+/**
+ * Recipe Class
+ * 
+ * Objects for storing and retrieving information about a recipe.
+ * 
+ * Instantiate with the three attributes name, description, and instructions.
+ * Change attributes with setAttribute methods.
+ * Return attributes with getAttribute methods.
+ * 
+ * Author: Mark Lange
+ * 
+*/
+public class Recipe {
+	
+    private String name;
+    private String description;
+    private String instructions;
+    
+    private Recipe(String name, String description, String instructions){	
+    	this.name         = name;
+    	this.description  = description;
+    	this.instructions = instructions;
+    } 
+    
+    public String getName(){
+    	return this.name;
+    }
+    
+    public String getDescription(){
+    	return this.description;
+    }
+    
+    public String getInstructions(){
+    	return this.instructions;
+    }
+    
+    public void setName(String name){
+    	this.name = name;
+    }
+    
+    public void setDescription(String description){
+    	this.description = description;
+    }
+    
+    public void setInstructions(String instructions){
+    	this.instructions = instructions;
+    }
+    
+}
